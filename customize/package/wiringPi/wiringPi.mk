d93e8c64d11dafaf77a2f8e81c7b7ff3565d6df4
hugh712 <hugh712@gmail.com>
2016-05-02 16:57:39 +0800

    rename patch name

--- customize/package/wiringPi/wiringPi.mk
+++ customize/package/wiringPi/wiringPi.mk
@@ -5,14 +5,11 @@
 ################################################################################
 
 WIRINGPI_VERSION = b0a60c3302973ca1878d149d61f2f612c8f27fac
-#WIRINGPI_VERSION ="" 
 WIRINGPI_SITE = git://git.drogon.net/wiringPi
 WIRINGPI_SITE_METHOD = git
 WIRINGPI_LICENSE = LGPLv2.1
 WIRINGPI_LICENSE_FILES = COPYING
-WIRINGPI_AUTORECONF = YES
 WIRINGPI_INSTALL_STAGING = YES
-WIRINGPI_CONF_OPTS = --disable-unittests
 
 
 define WIRINGPI_BUILD_CMDS
@@ -31,5 +28,4 @@ define WIRINGPI_INSTALL_STAGING_CMDS
 	$(INSTALL) -m 0755 -D $(@D)/wiringPi/libwiringPi.so.2.32          $(STAGING_DIR)/lib
 	ln -sf $(STAGING_DIR)/lib/libwiringPi.so.2.32 $(STAGING_DIR)/lib/libwiringPi.so	
 endef
-#$(eval $(autotools-package))
 $(eval $(generic-package))
